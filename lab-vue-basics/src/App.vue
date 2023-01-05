0398e4aff03ff52502ba28e928f0d31044d5ae3b
Ferran Calpe <ferran.calpe@gmail.com>
2023-01-05 18:56:07 +0100

    iteration 2 and 3

--- lab-vue-basics/src/App.vue
+++ lab-vue-basics/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <navbar/>
  <p>this is a mathematical operation 2+2={{2+2}}</p>
+ {{ stringAsAFunction() }}
   <footer/>
 </template>
 
@@ -9,18 +10,17 @@ import footer from './components/footer.vue'
 import navbar from "./components/navbar.vue"
 import { Ref } from 'vue';
 
-const numerical1 = 2568;
-const numerical2 = 4775551132;
-bunchOfNumbers = numerical1*numerical2;
-function(){
-  console.log ("bunchOfNumbers");
+export default{ //en principio esto me sirve para hacer el stringAsAFunction(). No se como se conectan por eso.
+  mounted() {
+    return{
+      message: "this is a string as a function"
+    }
+  }
 }
 
-v-if ("bunchOfNumbers">100000000) {
-  console.log ("that's a pretty big number.")
-}else{
-  console.log ("bunchOfNumbers")
-};
+//iteration 3
+
+
 </script>
 
 <style scoped>
