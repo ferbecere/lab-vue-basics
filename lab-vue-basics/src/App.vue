95e9119bd966030e97c843cd954c8e4ae0ddbc71
Ferran Calpe <ferran.calpe@gmail.com>
2023-01-05 19:39:16 +0100

    cierre

--- lab-vue-basics/src/App.vue
+++ lab-vue-basics/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <navbar/>
- <p>this is a mathematical operation 2+2={{2+2}}</p>
- {{ stringAsAFunction() }}
-  <footer/>
+  <navbar />
+  <p>this is a mathematical operation 2+2={{ 2 + 2 }}</p>
+  <p>{{ stringAsAFunction() }}</p>
+  <button @click="sayHi">salute!</button>
+  <p v-if="showMessage">Well Hello!</p>
+  <div v-for="post in posts" :key="post.title">
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.description }}</p>
+    <p>{{ post.content }}</p>
+  </div>
+  <footer />
 </template>
 
 <script setup>
-import footer from './components/footer.vue'
-import navbar from "./components/navbar.vue"
-import { Ref } from 'vue';
+import footer from "./components/footer.vue";
+import navbar from "./components/navbar.vue";
 
-export default{ //en principio esto me sirve para hacer el stringAsAFunction(). No se como se conectan por eso.
-  mounted() {
-    return{
-      message: "this is a string as a function"
-    }
-  }
+const posts = $ref([
+  {
+    title: "This is a title",
+    description: "This is a description",
+    content: "this is te content",
+  },
+  {
+    title: "This is a title 2",
+    description: "This is a description 2",
+    content: "this is te content 2",
+  },
+  {
+    title: "This is a title 3",
+    description: "This is a description 3",
+    content: "this is te content 3",
+  },
+]);
+
+function sayHello() {
+  console.log("hello");
 }
 
-//iteration 3
-
-
+sayHello();
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
